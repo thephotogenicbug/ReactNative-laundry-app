15637085ad0e1add40d8b9096cf74af4127099d1
thephotogenicbug <navinkumarachari@gmail.com>
2023-06-04 18:04:31 +0530

    import ProductReducer inside store

--- store.js
+++ store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CartReducer from "./CartReducer";
+import productReducer from "./productReducer";
 
 export default configureStore({
-    reducer:{
-        cart:CartReducer
-    }
-})
+  reducer: {
+    cart: CartReducer,
+    product: productReducer,
+  },
+});
