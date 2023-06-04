4846a84aeebb45751bce5bd26ed69ad1b01b9ef3
thephotogenicbug <navinkumarachari@gmail.com>
2023-06-04 16:30:46 +0530

    Design Profile and Current Location UI

---
+++ screens/HomeScreen.js
@@ -0,0 +1,97 @@
+import { Alert, Pressable, StyleSheet, Text, View, Image } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { Ionicons } from "@expo/vector-icons";
+
+const HomeScreen = () => {
+  const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
+    "we are loading your location"
+  );
+  const [locationServicesEnabled, setLocationServicesEnabled] = useState(false);
+
+  useEffect(() => {
+    checkIfLocationEnabled();
+    getCurrentLocation();
+  }, []);
+
+  const checkIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+    if (!enabled) {
+      Alert.alert(
+        "Location services not enabled",
+        "Please enable the location services",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ],
+        { cancelable: false }
+      );
+    } else {
+      setLocationServicesEnabled(enabled);
+    }
+  };
+
+  const getCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission denied",
+        "allow the app to use the location services",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ],
+        { cancelable: false }
+      );
+    }
+    const { coords } = await Location.getCurrentPositionAsync();
+    console.log(coords);
+    if (coords) {
+      const { latitude, longitude } = coords;
+
+      let response = await Location.reverseGeocodeAsync({
+        longitude,
+        latitude,
+      });
+      //   console.log(response);
+
+      for (let item of response) {
+        let address = `${item.name} ${item.city} ${item.postalCode}`;
+        setDisplayCurrentAddress(address);
+      }
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      {/* Profile and Location UI */}
+      <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+        <Ionicons name="location" size={30} color="#E32636" />
+        <View>
+          <Text style={{ fontSize: 18, fontWeight: "600" }}>Home</Text>
+          <Text>{displayCurrentAddress}</Text>
+        </View>
+        <Pressable style={{ marginLeft: "auto", marginRight: 7 }}>
+          <Image
+            style={{ width: 40, height: 40, borderRadius: 20 }}
+            source={{
+              uri: "https://res.cloudinary.com/dv5jjlsd7/image/upload/v1676913433/alexander-hipp-iEEBWgY_6lA-unsplash_ujzf0t.jpg",
+            }}
+          />
+        </Pressable>
+      </View>
+      {/* Profile and Location UI */}
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
